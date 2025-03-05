c0ab07bd6c64b2ea8c729e8bc49f92b4021fc0a0
Cauã R. Pereira <7aauac@gmail.com>
2025-03-05 03:20:22 -0300

    :wrench: chore: Configura docker-compose e vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: Number(process.env.APP_PORT ?? '5173'),
+    watch: {
+      usePolling: true,
+    },
+    strictPort: true,
+    hmr: {
+      clientPort: Number(process.env.APP_PORT ?? '5173')
+    },
+  },
 })
