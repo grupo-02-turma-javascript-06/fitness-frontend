d93361249af450636da498481af41ed11424982d
Cauã R. Pereira <7aauac@gmail.com>
2025-03-06 16:00:52 -0300

    :sparkles: feat: Adicionado navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import LoginCadastroForm from './pages/logincadastro/LoginCadastroForm';
 import { AuthProvider } from './contexts/AuthContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
     return (
@@ -10,13 +13,16 @@ function App() {
             <AuthProvider>
                 <ToastContainer />
                 <BrowserRouter>
+                    <Navbar />
                     <div className="min-h-[80vh]">
                         <Routes>
                             <Route path="/" element={<LoginCadastroForm />} />
                             <Route path="/login" element={<LoginCadastroForm />} />
                             <Route path="/cadastro" element={<LoginCadastroForm />} />
+                            <Route path='/home' element={<Home />} />
                         </Routes>
                     </div>
+                    <Footer />
                 </BrowserRouter>
             </AuthProvider>
         </>
