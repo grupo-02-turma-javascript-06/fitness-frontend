8fc416a5b2690777713f32c1c169ee0aa2694a8a
josifermaodev <josianefermao@gmail.com>
2025-03-07 12:40:27 -0300

    :lipstick: feat: Cria estilização para o componente lista categoria

--- src/components/categorias/listacategorias/ListaCategorias.tsx
+++ src/components/categorias/listacategorias/ListaCategorias.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { DNA } from "react-loader-spinner";
-import { MagnifyingGlass, Plus, ArrowRight } from "@phosphor-icons/react";
+import { MagnifyingGlass, Plus } from "@phosphor-icons/react";
 import CardCategorias from "../cardcategorias/CardCategorias";
 import { buscar } from "../../../services/Service";
 import Categoria from "../../../models/Categoria";
@@ -41,57 +41,71 @@ function ListaCategorias() {
 
   return (
     <>
-      {categorias.length === 0 && (
-        <DNA
-          visible={true}
-          height="200"
-          width="200"
-          ariaLabel="dna-loading"
-          wrapperStyle={{}}
-          wrapperClass="dna-wrapper mx-auto"
-        />
-      )}
-        <div className="w-full">
-          <div className="p-8">
-            <div className="flex flex-col md:flex-row justify-between items-center mb-6">
-              <h2 className="text-2xl font-bold text-center mb-4">
-                Lista de Categorias
-              </h2>
-              <div className="flex flex-col md:flex-row gap-3">
-                <div className="flex justify-center">
-                  <div className="relative">
-                    <input
-                      type="text"
-                      placeholder="Pesquisar..."
-                      className="bg-white rounded-lg pl-10 pr-3 py-2 text-gray-700 focus:outline-white"
-                    />
-                    <MagnifyingGlass
-                      className="absolute left-3 top-2.5 text-gray-400"
-                      size={20}
-                    />
-                  </div>
-                  <button className="ml-2 flex text-center items-center bg-[#FD6101] text-white px-3 py-2 rounded-lg hover:bg-[#B63700] duration-500">
-                    <ArrowRight size={20} />
-                  </button>
-                </div>
-                <button className="flex items-center justify-center gap-2 border-2 text-[#FD6101] px-4 py-2 rounded-lg hover:bg-[#FD6101] hover:text-white hover:border-[#FD6101] duration-500">
-                  <Link to='/categorias/store'>
-                    <Plus size={20} />
-                    Categoria
-                  </Link>
-                  
-                </button>
-              </div>
+      <div className="mt-15 flex flex-col gap-12">
+        <div className="flex flex-col justify-center items-center h-20 text-lg mx-4 md:mx-0">
+          <div className="container flex flex-col lg:flex-row justify-center items-center gap-5  mt-15 md:mt-0 w-full ">
+            <div className="w-full text-center md:w-[30%]">
+              <h2 className="text-3xl">Lista de Categorias</h2>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            <div className="w-full md:w-[70%]">
+              <form className="flex flex-col md:flex-row items-center mx-auto w-full gap-4 ">
+                <div className="w-full md:w-[70%]">
+                  <label htmlFor="search" className="sr-only">
+                    Search
+                  </label>
+                  <div className="flex gap-4 items-center bg-white w-full rounded-lg drop-shadow-lg">
+                    <div className="flex justify-center items-center bg-[#dbdada] w-15 h-11 rounded-l-lg">
+                      <MagnifyingGlass size={25} className="text-white" />
+                    </div>
+
+                    <input
+                      type="text"
+                      id="simple-search"
+                      className="w-full border-0 focus:ring-0 focus:outline-none py-2 px-4"
+                      placeholder="Pesquisar Exercício"
+                    />
+                  </div>
+                </div>
+                <div className="w-full md:w-[20%]">
+                  <button className="flex items-center justify-center border-2 text-[#FD6101] rounded-lg hover:bg-[#FD6101] hover:text-white hover:border-[#FD6101] duration-500 w-full py-1 px-6">
+                    <Link to='/categorias/store' className="flex justify-center items-center gap-3 w-full">
+                      <Plus size={20} />
+                      Categoria
+                    </Link>
+                    
+                  </button>
+                </div>
+
+              </form>
+            </div>
+          </div>
+        </div>
+        {categorias.length === 0 && (
+          <DNA
+            visible={true}
+            height="200"
+            width="200"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper mx-auto"
+          />
+        )}
+        <div className="flex justify-center w-full my-4 md:mt-0">
+          <div className="container flex flex-col">
+            {categorias.length === 0 && (
+              <span className="text-3xl text-center my-8">Nenhum exercício foi encontrado</span>
+            )}
+            <div
+              className="container mx-auto my-4 
+                          grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {categorias.map((categoria) => (
                 <CardCategorias key={categoria.id} categoria={categoria} />
               ))}
             </div>
           </div>
         </div>
-      
+      </div>
     </>
   );
 }
